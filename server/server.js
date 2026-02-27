11b43c1cb8b7cf82b334e75019ca59bf79dad6df
Sahana <sahanasahana64899@gmail.com>
2026-02-27 22:06:35 +0530

    Add Vercel frontend URL to CORS allowed origins

--- server/server.js
+++ server/server.js
@@ -44,6 +44,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:5174',
   'http://127.0.0.1:5174',
+  'https://sankar-electronics-ecommerce.vercel.app',
 ];
 
 app.use(cors({
